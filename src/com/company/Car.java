660254c27ddc7f3bea4b46d6226614b8f299d624
Fabian <fabian13pl@gmail.com>
2020-09-20 17:14:09 +0200

    upgrade car

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,9 +1,93 @@
 package com.company;
 
 public class Car {
-    String brand;
-    String color;
-    String segment;
-    int value;
-    int car_mileage;
+    private String brand;
+    private String segment;
+    private String color;
+    private int value;
+    private int car_mileage;
+    private boolean delivery;
+    private int storage;
+
+
+    //false -> broken
+    boolean Brakes = true;
+    boolean Suspension = true;
+    boolean Engine = true;
+    boolean Body = true;
+    boolean Transmission = true;
+
+    public void setDelivery(boolean delivery, int storage){
+        this.delivery=delivery;
+        this.storage=storage;
+    }
+
+    public boolean getDelivery() {
+        return delivery;
+    }
+
+    public int getStorage(){
+        return storage;
+    }
+
+    public void setBrand(String brand){
+        this.brand=brand;
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setSegment(String segment){
+        this.segment=segment;
+    }
+    public String getSegment(){
+        return segment;
+    }
+    public void setValue(int value){
+        this.value=value;
+    }
+    public int getValue(){
+        return value;
+    }
+    public void setCar_mileage(int car_mileage){
+        this.car_mileage=car_mileage;
+    }
+    public int getCar_mileage(){
+        return car_mileage;
+    }
+    public void setBrakes(boolean brakes){
+        this.Brakes=brakes;
+    }
+    public boolean getBreakes(){
+        return Brakes;
+    }
+    public void setSuspension(boolean suspension){
+        this.Suspension=suspension;
+    }
+    public boolean getSuspension(){
+        return Suspension;
+    }
+    public void setEngine(boolean engine){
+        this.Engine=engine;
+    }
+    public boolean getEngine(){
+        return Engine;
+    }
+    public void setBody(boolean body){
+        this.Body=body;
+    }
+    public boolean getBody(){
+        return Body;
+    }
+    public void setTransmission(boolean transmission){
+        this.Transmission=transmission;
+    }
+    public boolean getTransmission(){
+        return Transmission;
+    }
 }
--- src/com/company/Car_generator.java
+++ src/com/company/Car_generator.java
@@ -20,24 +20,79 @@ class CarGenerator {
         segment[1]="standart";
         segment[2]="budget";
 
-        int rn = ThreadLocalRandom.current().nextInt(0,3);
+        int rn = ThreadLocalRandom.current()
+                .nextInt(0,3);
 
         Car car = new Car();
-        car.brand=brand[rn];
-        car.color=color[rn];
-        car.segment=segment[rn];
+        car.setBrand(brand[ThreadLocalRandom.current()
+                .nextInt(0,3)]);
+        car.setColor(color[ThreadLocalRandom.current()
+                .nextInt(0,3)]);
+        car.setSegment(segment[rn]);
         switch (segment[rn]){
             case "premium":
-                car.value = ThreadLocalRandom.current().nextInt(30000,70000);
+                car.setValue(ThreadLocalRandom.current()
+                        .nextInt(30000,70000));
+                if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<2) {
+                    car.setBrakes(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<2) {
+                    car.setSuspension(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<2) {
+                    car.setEngine(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<2) {
+                    car.setBody(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<2) {
+                    car.setTransmission(false);
+                }
                 break;
             case "standard":
-                car.value = ThreadLocalRandom.current().nextInt(15000,30000);
+                car.setValue(ThreadLocalRandom.current()
+                        .nextInt(15000,30000));
+                if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<5) {
+                    car.setBrakes(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<5) {
+                    car.setSuspension(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<5) {
+                    car.setEngine(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<5) {
+                    car.setBody(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<5) {
+                    car.setTransmission(false);
+                }
                 break;
             case "budget":
-                car.value = ThreadLocalRandom.current().nextInt(1000,15000);
+                car.setValue(ThreadLocalRandom.current()
+                        .nextInt(1000,15000));
+                if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<7) {
+                    car.setBrakes(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<7) {
+                    car.setSuspension(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<7) {
+                    car.setEngine(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<7) {
+                    car.setBody(false);
+                }if(ThreadLocalRandom.current()
+                        .nextInt(0,10)<7) {
+                    car.setTransmission(false);
+                }
                 break;
         }
-        car.car_mileage= ThreadLocalRandom.current().nextInt(20000,200000);
+        car.setCar_mileage(ThreadLocalRandom.current()
+                .nextInt(20000,200000));
         return  car;
     }
 }
